f845ce6289686a1b87d04572ad1d7e4595e57c7d
PerfetiaNKazadi16 <kperfetia@gmail.com>
2026-05-28 16:01:54 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System;
+using System.Windows.Documents;
+
+namespace SecureBotWPF
+{
+   
+    public partial class MainWindow : Window
+    {
+        private Chatbot chatbot= new Chatbot();
+        private User user = new User();
+        public MainWindow()
+        {
+            InitializeComponent();
+            user.Name = "Perfetia";
+            VoiceGreeting.PlayGreeting();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string input = message_txt.Text;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                MessageBox.Show("Please type a message.");
+                return;
+
+            }
+            txtChat.Text = "You:" + input + "\n";
+            string response = chatbot.GetResponse(input, user);
+            txtChat.Text += "Bot:" + response + "\n\n";
+
+            message_txt.Clear();
+
+        }
+      
+    }
+}
---
+++ sentiment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureBotWPF
+{
+    class sentiment
+    {
+        public string DetectSentiment(string input)
+        {
+            input = input.ToLower();
+
+            if (input.Contains("worried"))
+                return "worried";
+
+            if (input.Contains("scared"))
+                return "worried";
+
+            if (input.Contains("frustrated"))
+                return "frustrated";
+
+            if (input.Contains("curious"))
+                return "curious";
+
+            return "neutral";
+        }
+    }
+}
+    }
+}
