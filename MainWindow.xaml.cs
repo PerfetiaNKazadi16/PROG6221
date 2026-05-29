3e951c095b5f1eb9b7acf742b63ca8e7b42e33c3
PerfetiaNKazadi16 <kperfetia@gmail.com>
2026-05-29 14:06:39 +0200

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,17 +5,23 @@ using System.Windows.Data;
 using System;
 using System.Windows.Documents;
 
+
 namespace SecureBotWPF
 {
-   
+  
     public partial class MainWindow : Window
     {
+         
+
         private Chatbot chatbot= new Chatbot();
         private User user = new User();
+        private bool waitingForName = true;
         public MainWindow()
         {
+            
             InitializeComponent();
-            user.Name = "Perfetia";
+            txtChat.Text =
+                 "Bot: Hello welcome to securebot your cybersecurity chat bot.\n" + "Bot:What is your name?";
             VoiceGreeting.PlayGreeting();
         }
 
@@ -30,6 +36,15 @@ namespace SecureBotWPF
 
             }
             txtChat.Text = "You:" + input + "\n";
+            if (waitingForName)
+            {
+                user.Name = input;
+                txtChat.Text += "Bot: Nice to meet you," + user.Name + "!\n" +
+                    "Bot: Ask me anything about cybersecurity.\n\n";
+                waitingForName = false;
+                message_txt.Clear();
+                return;
+            }
             string response = chatbot.GetResponse(input, user);
             txtChat.Text += "Bot:" + response + "\n\n";
 
